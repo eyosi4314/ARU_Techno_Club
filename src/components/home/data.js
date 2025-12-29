5c6bd316e7f3fcc794995b17dd8827f4f7dbd3ce
eyosi4314 <eyosi4314@gmail.com>
2025-12-30 01:39:03 +0300

    update home images

--- src/components/home/data.js
+++ src/components/home/data.js
@@ -1,6 +1,6 @@
-import bg_1 from "./img/arsiun_ardu_campus.jpg";
-import bg_2 from "./img/arsiun_green_area3.jfif";
-import bg_3 from "./img/arsiun_health_campus.jpg";
-import bg_5 from "./img/arsiun_health_campus2.jfif";
+import bg_1 from "./img/10001.jpg";
+import bg_2 from "./img/arsiun_health_campus.jpg";
+import bg_3 from "./img/10002.jpg";
+import bg_5 from "./img/arsiun_ardu_campus.jpg";
 
 export const img = [ bg_1, bg_2, bg_3, bg_5 ];
